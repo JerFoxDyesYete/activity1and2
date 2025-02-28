1f25250946f84c692f5111370a67a107525c30f0
JerfoxDyesYete <your.email@example.com>
2025-02-28 08:11:18 +0800

    empty-models for simplicity

--- app/Models/teachers.php
+++ app/Models/teachers.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Teachers extends Authenticatable
 {
-//
+    //
 }
